9ddf5ccb9f9e4cf25c6653372d9b342275027406
mellownightpirate <a@hasan.co>
2024-07-22 22:23:17 +0100

    conditional statements

--- main.cpp
+++ main.cpp
@@ -3,17 +3,64 @@ using namespace std;
 
 int Health{ 150 };
 bool isDead{ false };
+bool hasLoot{ true };
+int Exp{ 500 };
+bool canHeal{ true };
+bool shouldAwardExperience{ true };
+bool canSeePlayer{ true };
+bool isHostile{ false };
+
+void DropLoot() {
+	hasLoot = false;
+	cout << "You lost all of your loot.\n";
+}
+
+void RunAway() {
+	cout << "Run!!.";
+}
+
+void Attack() {
+	cout << "Attack!!";
+}
+
+void AwardExperience() {
+	cout << "Just gained " << 50 << " EXP.\n";
+	Exp += 50;
+	cout << "Total EXP " << Exp;
+}
+
+void CastHealingSpell() {
+	cout << "Drank 'Elixir of Life'!\n";
+	Health += 50;
+	cout << "Your health just improved by " << Health << " points.";
+}
+
+void PatrolArea() {
+	cout << "On the lookout!";
+}
 
 void TakeDamage() {
-	cout << " - TakeDamage function starting\n";
-	Health -= 50;
-	cout << " - TakeDamage function complete\n";
+	if (Health <= 0) {
+		isDead = true;
+		if (hasLoot) {
+			DropLoot();
+		}
+		if (shouldAwardExperience) {
+			AwardExperience();
+		}
+	}
+	else if (Health <= 50) {
+		canHeal ? CastHealingSpell() : RunAway();
+	}
+	else if (isHostile) {
+		canSeePlayer ? Attack() : PatrolArea();
+	}
+	else {
+		cout << "Taking damage but not in critical condition or hostile.\n";
+	}
 }
 
 int main() {
-	cout << "Main function starting\n";
+	Health -= 150;
 	TakeDamage();
-	cout << "We are back in main\n";
-	TakeDamage();
-	cout << "Main function complete\n";
 }
