d7deaf70317c263b1a63762880017550682c19c3
mellownightpirate <a@hasan.co>
2024-07-25 21:23:52 +0100

    strong function return types

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int Day{ 3 };
+int CalculateLevel() {
+	return 5;
+};
 
 int main() {
-	switch (Day) {
-	case 1:
-		cout << "Monday";
-		break;
-	case 2:
-		cout << "Tuesday";
-		break;
-	default:
-		cout << "Something else";
-	}
+	int Level{ CalculateLevel() };
+	cout << "Level: " << Level;
 }
