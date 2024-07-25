16cb910652083546cbb663b0e299336cba01fae8
mellownightpirate <a@hasan.co>
2024-07-26 00:55:50 +0100

    optional parameters

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int Health{ 150 };
 
-void TakeDamage(int Damage) {
+void TakeDamage(
+	int Damage, bool isMagical = false
+) {
 	cout << "Inflicting " << Damage << " Damage";
-	Health -= Damage;
+	// Magical damage is doubled
+	Health -= isMagical ? Damage * 2 : Damage;
 	cout << " - Health: " << Health << '\n';
 }
 
 int main() {
-	TakeDamage(20);
+	TakeDamage(10, true);
 	TakeDamage(50);
 	TakeDamage(70);
 }
