a847a55579a89bfa04ebb732c79c6b1464b78d91
mellownightpirate <a@hasan.co>
2024-07-27 15:41:04 +0100

    lesson 19

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// generate a programme that iterates 1 throught to 100,
-// but outputs fizz for every iteration divisible by three instead of the number,
-// buzz for every iteration divisble by five instead of the number,
-// fizzbuzz for every iteration divisble by 15 instead of the number.
-
-
 int main() {
-
-	int Number{ 0 };
-
-	for (int i{ 1 }; i <= 100; i++) {
-		++Number;
-		if (Number % 3 == 0) {
-			cout << "Fizz, ";
-		} if (Number % 5 == 0) {
-			cout << "Buzz, ";
-		} if (Number % 15 == 0) {
-			cout << "FizzBuzz, ";
-		} if (Number == 100) {
-			cout << " FizzBuzz.";
-		}
-		else cout << Number << ", ";
-	};
-
+    for (int i = 1; i <= 100; i++) {
+        cout << (i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : to_string(i));
+        cout << (i < 100 ? ", " : ".");
+    }
+    return 0;
 }
